5c5d80792d8a2b0624825e3f08dff944399c1665
Gabriel Lima <gabrielcastrolima35@gmail.com>
2016-11-05 21:54:30 -0200

    update sql initial script

--- create_invoices.sql
+++ create_invoices.sql
@@ -1,12 +1,12 @@
 CREATE DATABASE IF NOT EXISTS desafio_stone;
 
-CREATE TABLE IF NOT EXISTS `desafio_stone`.`invoices` (
+CREATE TABLE IF NOT EXISTS `database`.`invoices` (
   `document` VARCHAR(14) NOT NULL,
   `description` VARCHAR(256),
-  `amount` DECIMAL(16,2),
-  `reference_month` INT,
-  `reference_year` INT,
-  `created_at` DATETIME(0) ,
-  `is_active` TINYINT(1),
+  `amount` DECIMAL(16,2) NOT NULL,
+  `reference_month` INT NOT NULL,
+  `reference_year` INT NOT NULL,
+  `created_at` DATETIME(0) NOT NULL,
+  `is_active` TINYINT(1) NOT NULL,
   `desactive_at` DATETIME(0),
   PRIMARY KEY (`document`));
