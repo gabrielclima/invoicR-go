759b124a88502647639b688fd7eaa41f6f936ccc
Gabriel Lima <gabrielcastrolima35@gmail.com>
2016-11-05 21:59:45 -0200

    change invoice description datatype

--- invoice.go
+++ invoice.go
@@ -5,14 +5,14 @@ import (
 )
 
 type Invoice struct {
-	Document        int       `json:"document"`
-	Description     string    `json:"description"`
-	Amount          float64   `json:"amount"`
-	ReferenceMounth int       `json:"reference_mounth"`
-	ReferenceYear   int       `json:"reference_year"`
-	CreatedAt       time.Time `json:"created_at"`
-	IsActice        byte      `json:"is_active"`
-	DesactiveAt     NullTime  `json:"desactive_at"`
+	Document        int            `json:"document"`
+	Description     sql.NullString `json:"description"`
+	Amount          float64        `json:"amount"`
+	ReferenceMounth int            `json:"reference_mounth"`
+	ReferenceYear   int            `json:"reference_year"`
+	CreatedAt       time.Time      `json:"created_at"`
+	IsActice        byte           `json:"is_active"`
+	DesactiveAt     NullTime       `json:"desactive_at"`
 }
 
 type Invoices []Invoice
