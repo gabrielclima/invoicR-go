23aad23ab24a6e376f73667358e52c9c08116045
Gabriel Lima <gabrielcastrolima35@gmail.com>
2016-11-06 01:19:42 -0200

    endpont create invoice

--- rest.go
+++ rest.go
@@ -1,17 +1,21 @@
 package main
 
 import (
-	"net/http"
 	"encoding/json"
+	// "fmt"
+	// "log"
+	"io/ioutil"
+	"net/http"
 )
 
 func RestInvoices(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+
 	invoices := GetAllInvoices()
 	res, err := json.Marshal(invoices)
 	checkErr(err)
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 
-	if invoices == nil{
+	if invoices == nil {
 		w.WriteHeader(http.StatusNotFound)
 		res, err = json.Marshal(jsonErr{Code: http.StatusNotFound, Text: "Not Found"})
 		checkErr(err)
@@ -20,8 +24,39 @@ func RestInvoices(w http.ResponseWriter, r *http.Request) {
 	w.Write(res)
 }
 
-func checkErr(err error) {
-    if err != nil {
-        panic(err)
-    }
+type test_struct struct {
+	Test string
+}
+
+func RestCreateInvoice(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+
+	var err error
+	var res []byte
+
+	invoice := new(Invoice)
+	body, err := ioutil.ReadAll(r.Body)
+	checkErr(err)
+
+	err = json.Unmarshal(body, &invoice)
+	checkErr(err)
+
+	inv := GetInvoiceByDoc(invoice.Document)
+
+	if inv != (Invoice{}) {
+		w.WriteHeader(http.StatusConflict)
+		res, err = json.Marshal(jsonErr{Code: http.StatusConflict,
+			Text: "Já existe um documento com esse número"})
+		checkErr(err)
+    w.Write(res)
+
+    return
+  } else {
+    inv := CreateInvoice(invoice)
+    res, err = json.Marshal(inv)
+    checkErr(err)
+    w.WriteHeader(http.StatusCreated)
+
+    w.Write(res)
+  }
 }
