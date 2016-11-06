ae7e7508f8547982d3cb1a3a0bed57b8f46154ec
Gabriel Lima <gabrielcastrolima35@gmail.com>
2016-11-06 13:04:24 -0200

    filtering and ordering by request params

--- services.go
+++ services.go
@@ -1,19 +1,47 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-
 	_ "github.com/go-sql-driver/mysql"
+	"strings"
+	// "log"
 )
 
-func GetAllInvoices() Invoices {
+func GetAllInvoices(params map[string][]string) Invoices {
+	fmt.Println(params)
+
+	orderBy := strings.Join(params["orderBy"], "")
+	year := strings.Join(params["year"], "")
+	month := strings.Join(params["month"], "")
+
 	var invoice Invoice
 	var invoices Invoices
 
 	var sql = "select * from invoices"
-	sql += " where document = ?"
+
+	if year != "" {
+		sql += "  where reference_year = " + year
+
+		if month != "" {
+			sql += "  and reference_month = " + month
+		}
+
+	} else {
+
+		if month != "" {
+			sql += " where reference_month = " + month
+		}
+
+	}
+
+	if orderBy != "" {
+		sql += " order by " + orderBy
+	}
+
 	fmt.Println(sql)
-	rows, err := db.Query(sql, "12312310")
+
+	rows, err := db.Query(sql)
 	checkErr(err)
 
 	for rows.Next() {
@@ -29,6 +57,23 @@ func GetAllInvoices() Invoices {
 	return invoices
 }
 
+func GetInvoiceByDoc(document int) Invoice {
+	var invoice Invoice
+	stmt, err := db.Prepare("select * from invoices where document=?")
+	checkErr(err)
+	// row := db.QueryRow("select * from invoices where document=?;", document)
+	err = stmt.QueryRow(document).Scan(&invoice.Document, &invoice.Description, &invoice.Amount,
+		&invoice.ReferenceMounth, &invoice.ReferenceYear,
+		&invoice.CreatedAt, &invoice.IsActice, &invoice.DesactiveAt)
+	if err == sql.ErrNoRows {
+		return invoice
+	}
+	checkErr(err)
+	defer stmt.Close()
+
+	return invoice
+}
+
 func CreateInvoice(invoice *Invoice) *Invoice {
 	var sql = "insert into invoices set document=?, description=?, amount=?,"
 	sql += " reference_month=?, reference_year=?, created_at=NOW(), is_active=1"
@@ -36,11 +81,23 @@ func CreateInvoice(invoice *Invoice) *Invoice {
 	checkErr(err)
 
 	_, err = stmt.Exec(invoice.Document, invoice.Description, invoice.Amount,
-	invoice.ReferenceMounth, invoice.ReferenceYear)
+		invoice.ReferenceMounth, invoice.ReferenceYear)
+	checkErr(err)
 
 	defer stmt.Close()
 
 	return invoice
+}
+
+func DeleteInvoice(document int) string {
+	stmt, err := db.Prepare("update invoices set is_active=0, desactive_at=NOW() where document = ?")
+	checkErr(err)
+
+	_, err = stmt.Exec(document)
+	checkErr(err)
+	defer stmt.Close()
+
+	return "deleted"
 }
 
 // func GetInvoiceByDocument(int document) Invoice {}
