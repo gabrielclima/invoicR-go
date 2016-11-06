1ebcfe720f3833e778ea25e6c93df2489eb993f5
Gabriel Lima <gabrielcastrolima35@gmail.com>
2016-11-06 01:40:37 -0200

    get invoice by doc endpoint

--- main.go
+++ main.go
@@ -13,11 +13,10 @@ func main() {
 	defer db.Close()
 	router := mux.NewRouter().StrictSlash(true)
 
-	// declarando rotas seus respectivos métodos http e as funções a serem executadas
 	router.HandleFunc("/invoices", RestInvoices).Methods("GET")
-	// router.HandleFunc("/invoices", RestCreateInvoice).Methods("POST")
-	// router.HandleFunc("/invoices", RestInvoiceByDoc).Methods("GET")
-	// router.HandleFunc("/invoices/:document", RestDeleteInvoices).Methods("DELETE")
+	router.HandleFunc("/invoices", RestCreateInvoice).Methods("POST")
+	router.HandleFunc("/invoices/{document}", RestInvoiceByDoc).Methods("GET")
+	// router.HandleFunc("/invoices/{document}", RestDeleteInvoice).Methods("DELETE")
 
 	// colocando a API online
 	log.Fatal(http.ListenAndServe(":8080", router))
