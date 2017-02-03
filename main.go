88885f654b12d0d123c3318a679d44f5add14f00
Gabriel Lima <hdbiel@live.com>
2017-02-03 20:49:04 -0200

    tratando de chamar todos os defers quando a aplicação for encerrada

--- main.go
+++ main.go
@@ -5,8 +5,12 @@ import (
 	"github.com/gabrielclima/go_rest_api/rest"
 	"log"
 	"net/http"
+	"os"
+	"runtime"
 )
 
 func main() {
+	defer os.Exit(0)
 	log.Fatal(http.ListenAndServe(":8080", rest.Handlers()))
+	runtime.Goexit()
 }
