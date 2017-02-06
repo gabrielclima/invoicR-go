e85da32cfbf168440c85c12b24d022f0dba39fef
Gabriel Lima <hdbiel@live.com>
2017-02-06 17:34:31 -0200

    api resources with link for docs

--- rest/api_resources.go
+++ rest/api_resources.go
@@ -9,5 +9,11 @@ const TextHTML = "text/html; charset=UTF-8"
 
 func ApiResource(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", TextHTML)
-	fmt.Fprint(w, "Hello")
+	text := "<h2>Golang REST API</h2>"
+	text += "<span>Essa é uma API RESTFull escrita em Golang.</span>"
+	text += "<span>A documentação contendo todas as informações" +
+		"e as rotas disponíveis se encontra em " +
+		"<a href=\"https://github.com/gabrielclima/go_rest_api\">" +
+		"github.com/gabrielclima/go_rest_api</a>.</span>"
+	fmt.Fprint(w, text)
 }
